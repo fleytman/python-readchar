724697f50ba9851d81dd27c47139106bbd2b1d4d
Jan Wille <mail@janwille.de>
2022-07-11 15:16:38 +0200

    simplify windows scancode dict
    the windows scancodes consist of two byttes, the first denoting a
    special scancode and the second beeing the actuall key information.
    Previosly both bytes where used to calculate the number to look up in a
    dictionary, but as only the second byte is actually different, this can
    be simplifyed.
    
    closes #75, closes #66, closes #9, closes #8

--- readchar/readchar.py
+++ readchar/readchar.py
@@ -19,67 +19,42 @@ else:
 
 if sys.platform in ("win32", "cygwin"):
     #
-    # Windows uses scan codes for extended characters. The ordinal returned is
-    # 256 * the scan code.  This dictionary translates scan codes to the
-    # unicode sequences expected by readkey.
+    # Windows uses scan codes for extended characters. This dictionary translates
+    # scan codes to the unicode sequences expected by readkey.
     #
     # for windows scan codes see:
     #   https://msdn.microsoft.com/en-us/library/aa299374
     #      or
     #   http://www.quadibloc.com/comp/scan.htm
     xlate_dict = {
-        13: key.ENTER,
-        27: key.ESC,
-        15104: key.F1,
-        15328: key.F1,
-        15360: key.F2,
-        15584: key.F2,
-        15616: key.F3,
-        15840: key.F3,
-        15872: key.F4,
-        16096: key.F4,
-        16128: key.F5,
-        16352: key.F5,
-        16384: key.F6,
-        16608: key.F6,
-        16640: key.F7,
-        16864: key.F7,
-        16896: key.F8,
-        17120: key.F8,
-        17152: key.F9,
-        17376: key.F9,
-        17408: key.F10,
-        17632: key.F10,
-        34048: key.F11,
-        34272: key.F11,
-        34304: key.F12,
-        34528: key.F12,
+        59: key.F1,
+        60: key.F2,
+        61: key.F3,
+        62: key.F4,
+        63: key.F5,
+        64: key.F6,
+        65: key.F7,
+        66: key.F8,
+        67: key.F9,
+        68: key.F10,
+        133: key.F11,
+        134: key.F12,
         # don't have table entries for...
         # CTR_A, ..
         # ALT_A, ..
         # CTRL-F1, ..
         # CTRL_ALT_SUPR,
         # CTRL_ALT_A, .., etc.
-        20992: key.INSERT,
-        21216: key.INSERT,
-        21248: key.SUPR,  # key.py uses SUPR, not DELETE
-        21472: key.SUPR,  # key.py uses SUPR, not DELETE
-        18688: key.PAGE_UP,
-        18912: key.PAGE_UP,
-        20736: key.PAGE_DOWN,
-        20960: key.PAGE_DOWN,
-        18176: key.HOME,
-        18400: key.HOME,
-        20224: key.END,
-        20448: key.END,
-        18432: key.UP,
-        18656: key.UP,
-        20480: key.DOWN,
-        20704: key.DOWN,
-        19200: key.LEFT,
-        19424: key.LEFT,
-        19712: key.RIGHT,
-        19936: key.RIGHT,
+        82: key.INSERT,
+        83: key.SUPR,  # key.py uses SUPR, not DELETE
+        73: key.PAGE_UP,
+        81: key.PAGE_DOWN,
+        71: key.HOME,
+        79: key.END,
+        72: key.UP,
+        80: key.DOWN,
+        75: key.LEFT,
+        77: key.RIGHT,
     }
 
     def readkey(getchar_fn=None):
@@ -92,13 +67,10 @@ if sys.platform in ("win32", "cygwin"):
                 a = ord(ch)
                 if a == 0 or a == 224:
                     b = ord(msvcrt.getch())
-                    x = a + (b * 256)
-
                     try:
-                        return xlate_dict[x]
+                        return xlate_dict[b]
                     except KeyError:
                         return None
-                    return x
                 elif a == 8:
                     return key.BACKSPACE
                 elif a == 13:
